6237b80b4e414ef3e92f04c3bdfe7a150e03678c
Minaki1018 <yujidrone1018@outlook.com>
2017-03-28 16:17:25 +0900

    fixed

--- orb-slam/image/run.sh
+++ orb-slam/image/run.sh
@@ -2,5 +2,5 @@
 # image minaki/orb-slam:full
 docker run -it --rm minaki/orb-slam:full --net rosnet --name master --env ROS_HOSTNAME=master --env ROS_MASTER_URI=http://master:11311 --env DISPLAY=unix$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" zsh
 
-echo "docker run -it --rm orb-slam:core --net rosnet --name master --env ROS_HOSTNAME=master --env ROS_MASTER_URI=http://master:11311 --env DISPLAY=unix$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" bash"
+echo "docker run -it --rm orb-slam:core --net rosnet --name master --env ROS_HOSTNAME=master --env ROS_MASTER_URI=http://master:11311 --env DISPLAY=unix$DISPLAY --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" zsh"
 
