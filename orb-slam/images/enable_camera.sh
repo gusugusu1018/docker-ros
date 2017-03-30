62b610bfb44f2e0abc4913ff594a6a05edc9c8b9
minaminoki <sunahukinn1353@me.com>
2017-03-30 13:52:10 +0900

    fied

--- orb-slam/images/enable_camera.sh
+++ orb-slam/images/enable_camera.sh
@@ -3,6 +3,7 @@ if [ -e /dev/video0 ]; then
   echo "This script have to add sudo"
   sudo chmod a+rw /dev/video0
   echo "Enable camera (/dev/video0)"
+  ls -l /dev/video0
 else
   echo "/dev/video0 not found"
 fi
